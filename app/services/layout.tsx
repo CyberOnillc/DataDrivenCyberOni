cb25154f363c02293e4130a08747c71469428740
Tyrique Daniel <tyrique1.daniel@gmail.com>
2024-08-17 12:16:03 -0600

    update service card

--- app/services/layout.tsx
+++ app/services/layout.tsx
@@ -1,47 +1,42 @@
-import CompanyCarousel from "@/components/home/CompanyCarousel";
+import { seoUrl } from "@/lib/utils";
+import { Image as ServiceImage } from "@prisma/client";
 import { MoveRight } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { ReactNode } from "react";
 
-function ServiceLayout({ children }: { children: ReactNode }) {
-  return (
-    <>
-      <div>
-        <section className="container mx-auto flex flex-col lg:flex-row gap-10 justify-center items-center">
-          <div className="p-5 lg:w-1/3">
-            <div className="text-service-green text-sm lg:text-base">A TRUSTED COMPANY</div>
-            <h1 className="text-4xl lg:text-7xl font-bold">Next-Gen Software Services for Businesses.</h1>
-            <div className="border-l-4 border-service-green px-3 text-sm lg:text-base">Checkout Services We Offer Below!</div>
+export type ServiceCardProps = {
+    id: string;
+    title: string;
+    previewContent: string;
+    image: ServiceImage;
+};
+
+function ServiceCard({ id, image, previewContent, title }: ServiceCardProps) {
+    return (
+        <div className="flex flex-col justify-between rounded-xl shadow-lg hover:shadow-lg dark:shadow-[2px_3px_10px_0_#09090b] dark:hover:shadow-[2px_3px_40px_0_#09090b] dark:bg-zinc-900 p-5 h-full">
+            {image && (
+                <Image
+                    className="rounded-full h-10 w-10 mb-5"
+                    src={image.src}
+                    alt={title}
+                    height={30}
+                    width={30}
+                />
+            )}
+            <div className="border-l-4 border-service-green px-3 mb-5 font-bold text-xl whitespace-nowrap overflow-hidden text-ellipsis">
+                {title}
+            </div>
+            <div className="mb-5 text-sm line-clamp-3 flex-grow overflow-hidden">
+                {previewContent}
+            </div>
             <Link
-              href={'/schedule'}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="rounded-full p-3 bg-service-green text-white flex items-center w-fit gap-3 my-5 shadow-[0_10px_40px_0px_#33D3D454] text-sm lg:text-base"
+                href={`/services/${seoUrl(title, id)}#description`}
+                className="flex items-center gap-x-2 text-blue-500 mt-auto"
             >
-              Struggling? Book a Call <MoveRight size={20} />
+                Learn more <MoveRight size={16} />
             </Link>
-          </div>
-          <div className="lg:w-1/2 hidden lg:block">
-            <Image className="object-fill" src={'/images/service-hero.png'} alt="service-hero" height={500} width={500} />
-          </div>
-        </section>
-        <section className="container mx-auto">
-          <div className="container my-4">
-            <CompanyCarousel cards={companies}></CompanyCarousel>
-          </div>
-        </section>
-        {children}
-      </div>
-    </>
-  );
+        </div>
+    );
 }
 
-const companies = [
-  { name: 'google', image: '/images/Google cloud.png' },
-  { name: 'Ebay', image: '/images/Ebay.png' },
-  { name: 'Tux', image: '/images/Tux.png' },
-  { name: 'Spotify', image: '/images/Spotify.png' }
-];
-
-export default ServiceLayout;
+export default ServiceCard;
