7d06b42e6179407b131fad62e5771003b86f1ecf
Tyrique Daniel <tyrique1.daniel@gmail.com>
2024-08-17 00:43:46 -0600

    updated enterprise page

--- components/blogs/BlogCTA.tsx
+++ components/blogs/BlogCTA.tsx
@@ -25,12 +25,13 @@ function BlogCTA({ title, subTitle, link, button }: BlogCTAProps) {
       <div className="flex h-fit w-1/4 max-w-[25%] items-center justify-end text-ellipsis px-5">
         <Link
           href={link ? link : "/contact"}
-          className="line-clamp-1 min-w-16 max-w-md text-ellipsis h-full  rounded-full bg-white px-3 leading-10 text-center  text-sm font-semibold text-black hover:bg-gray-300"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="line-clamp-1 min-w-16 max-w-md text-ellipsis h-full rounded-full bg-white px-3 leading-10 text-center text-sm font-semibold text-black hover:bg-gray-300"
         >
-          {button
-            ? button
-            : "Get Started"}
+          {button ? button : "Get Started"}
         </Link>
+
       </div>
     </div>
   );
--- components/enterprise/HowItWorks.tsx
+++ components/enterprise/HowItWorks.tsx
@@ -5,20 +5,27 @@ export type StepCardProps = {
     content: string
 }
 
-function HowItWorks({ steps }: { steps:StepCardProps[] }) {
+import Link from 'next/link';
+
+function HowItWorks({ steps }: { steps: StepCardProps[] }) {
     return (
-        <div className="container mx-auto px-5 lg:px-10 flex flex-col items-center justify-center ">
+        <div className="container mx-auto px-5 lg:px-10 flex flex-col items-center justify-center">
             <h2 className="text-4xl font-bold">How it works</h2>
             <div>Explain how it works brief</div>
             <div className="flex flex-col lg:flex-row space-x-4 my-5">
-                {steps.map((step,index)=>(<StepCard key={index} icon={step.icon} content={step.content} />))}
+                {steps.map((step, index) => (
+                    <StepCard key={index} icon={step.icon} content={step.content} />
+                ))}
             </div>
-            <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full my-10">
-                Get Started
-            </button>
+            <Link href="/contact">
+                <a className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full my-10">
+                    Get Started
+                </a>
+            </Link>
         </div>
     );
 }
+
 
 function StepCard({ icon, content }: StepCardProps) {
     return (<div className="flex flex-col items-center ">
