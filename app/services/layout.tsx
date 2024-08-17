000f386bf96aee1ec64473b689d9caba60daf95e
Tyrique Daniel <tyrique1.daniel@gmail.com>
2024-08-16 22:53:17 -0600

    fixed service title wrap

--- app/services/layout.tsx
+++ app/services/layout.tsx
@@ -13,8 +13,8 @@ function ServiceLayout({ children }: { children: ReactNode }) {
             <section className="container mx-auto flex flex-col lg:flex-row  gap-10 justify-center items-center">
                 <div className="p-5 lg:w-1/3">
                     <div className="text-service-green">A TRUSTED COMPANY</div>
-                    <h1 className="text-7xl">Next-Gen Software Services to Businesses.</h1>
-                    <div className="border-l-4 border-service-green px-3">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</div>
+                    <h1 className="text-7xl">Next-Gen Software Services for Businesses.</h1>
+                    <div className="border-l-4 border-service-green px-3">Checkout Services We Offer Below!</div>
                     <Link href={'#'} className="rounded-full p-3 bg-service-green text-white flex items-center w-fit gap-3 my-5 shadow-[0_10px_40px_0px_#33D3D454]">Get Started <MoveRight /></Link>
 
                 </div>
