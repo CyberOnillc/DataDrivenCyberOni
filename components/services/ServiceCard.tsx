3dfa1b091f6b9574a327b1e9d28ae60a7eb00c8c
Tyrique Daniel <tyrique1.daniel@gmail.com>
2024-08-16 23:28:32 -0600

    replaced service layout

--- components/services/ServiceCard.tsx
+++ components/services/ServiceCard.tsx
@@ -15,13 +15,14 @@ function ServiceCard({ id, image, previewContent, title }: ServiceCardProps) {
     return (
         <>
             <div className="flex flex-col justify-around rounded-xl shadow-lg hover:shadow-lg dark:shadow-[2px_3px_10px_0_#09090b] dark:hover:shadow-[2px_3px_40px_0_#09090b] dark:bg-zinc-900 p-5 h-full">
-                <Image
+                {/*?commented out images to prevent alt
+                 <Image
                     className="rounded-full h-10 w-10 mb-5"
                     src={image ? image.src : "https://picsum.photos/200?random=1"}
                     alt={title}
                     height={30}
                     width={30}
-                />
+                /> */}
                 <div className="border-l-4 border-service-green px-3 mb-5 font-bold text-xl whitespace-nowrap overflow-hidden text-ellipsis">
                     {title}
                 </div>
