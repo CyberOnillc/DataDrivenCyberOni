12452317db428a096b2a464c598941ac11f385ed
Tyrique Daniel <tyrique1.daniel@gmail.com>
2024-08-28 20:25:22 -0600

    fixed redirect message

--- components/shared/Redirecting.tsx
+++ components/shared/Redirecting.tsx
@@ -2,18 +2,17 @@ import LoadingAnimation from "./icons/loading-animation";
 
 function Redirecting({ message }: { message: string }) {
     return (
-        <div className="grid h-screen place-content-center text-gray-900 dark:text-gray-100  bg-white dark:bg-slate-950 px-4 font-nunito">
+        <div className="grid h-screen place-content-center text-gray-900 dark:text-gray-100 bg-white dark:bg-slate-950 px-4 font-nunito">
             <div className="text-center">
-                {/* Lucide's XCircle icon */}
-                <div className="mx-auto h-5 w-full  sm:h-64">
+                {/* Container for the loading animation with adjusted height for mobile */}
+                <div className="mx-auto h-20 w-full sm:h-64">
                     <LoadingAnimation />
                 </div>
 
-                <h3 className="w- mt-6 text-xl font-bold tracking-tight sm:text-2xl">
+                {/* Ensure the message has appropriate spacing */}
+                <h3 className="mt-6 text-xl font-bold tracking-tight sm:text-2xl">
                     {message}
                 </h3>
-
-
             </div>
         </div>
     );
